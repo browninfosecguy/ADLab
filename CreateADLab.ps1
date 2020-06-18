1a35e04f460863cef6c7a1bbe741b0bff1ea3373
Sonny <37906998+browninfosecguy@users.noreply.github.com>
2020-06-17 17:29:51 -0700

    Create CreateADLab.ps1

---
+++ CreateADLab.ps1
@@ -0,0 +1,41 @@
+function Initialize-DomainController{
+    [CmdletBinding()]
+   
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$forestName,
+
+        [Parameter(Mandatory=$true)]
+        [string]$computerName
+
+
+
+    )
+
+    Begin{}
+    Process{}
+    End{}
+
+       
+}
+
+function Initialize-Workstation{
+    [CmdletBinding()]
+   
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$forestName,
+
+        [Parameter(Mandatory=$true)]
+        [string]$computerName
+
+
+
+    )
+
+    Begin{}
+    Process{}
+    End{}
+
+
+}
