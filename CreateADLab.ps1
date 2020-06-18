9126b932be46de701f40bd83370cca7e3b68fa2f
Sonny <37906998+browninfosecguy@users.noreply.github.com>
2020-06-18 10:09:14 -0700

    Update CreateADLab.ps1

--- CreateADLab.ps1
+++ CreateADLab.ps1
@@ -5,7 +5,7 @@ $osType = (Get-CimInstance -ClassName Win32_OperatingSystem).ProductType
 if($osType -eq 3)
 {
     Write-Host "Server install detected. Initializing Domain Controller configuration"
-    Initialize-DomainController
+    
 }
 elseif ($osType -eq 1) {
 
@@ -22,17 +22,18 @@ function Initialize-DomainController{
    
     param(
         [Parameter(Mandatory=$true)]
-        [string]$forestName,
+        [string]$forestName
 
-        [Parameter(Mandatory=$true)]
-        [string]$computerName
+        
 
 
 
     )
 
     Begin{Write-Host "Starting Domain Controller Configuration."}
-    Process{}
+    Process{
+        
+    }
     End{}
 
        
@@ -54,6 +55,41 @@ function Initialize-Workstation{
 
     Begin{}
     Process{}
+    End{}
+
+
+}
+
+function Set-PreConfig{
+    [CmdletBinding()]
+    Param(
+    [Parameter(Mandatory=$true)]
+    [string]$newComputerName
+
+    )
+    Begin{Write-Host "Changing Name of the Computer."}
+    Process{
+        
+        Rename-Computer -NewName $newComputerName -PassThru
+
+        $netInterface = Get-NetIPAddress -AddressFamily IPv4 | Select-Object IPv4Address,InterfaceIndex 
+
+        $netInterface
+
+        $selection = Read-Host "Select the InterfaceIndex for Primary Domain Controller"
+
+        $ipAddress = Read-Host "Enter the IP Address to assing to the interface"
+        $prefixLength = Read-Host "Enter Subnet Mask (For example enter 24 for Subnet mask 255.255.255.0)"
+        $defaultGateway = Read-Host "Enter Default gateway"
+        
+        
+        New-NetIPAddress -InterfaceIndex $selection -IPAddress $ipAddress -DefaultGateway $defaultGateway -PrefixLength $prefixLength
+
+        }
+
+
+
+    
     End{}
 
 
