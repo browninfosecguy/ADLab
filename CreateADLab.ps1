ffc64bf4aec9142cd5dd44f6a7c220a7fd6cd75d
Sonny <37906998+browninfosecguy@users.noreply.github.com>
2020-06-17 17:42:01 -0700

    Update CreateADLab.ps1

--- CreateADLab.ps1
+++ CreateADLab.ps1
@@ -1,3 +1,22 @@
+#Requires -RunAsAdministrator 
+
+$osType = (Get-CimInstance -ClassName Win32_OperatingSystem).ProductType
+
+if($osType -eq 3)
+{
+    Write-Host "Server install detected. Initializing Domain Controller configuration"
+    Initialize-DomainController
+}
+elseif ($osType -eq 1) {
+
+    Write-Host "Workstation install detected. Initializing workstation configuration"
+}
+else {
+    
+    Write-Host "Fatal Error. Cannot Proceed."
+}
+
+
 function Initialize-DomainController{
     [CmdletBinding()]
    
@@ -12,7 +31,7 @@ function Initialize-DomainController{
 
     )
 
-    Begin{}
+    Begin{Write-Host "Starting Domain Controller Configuration."}
     Process{}
     End{}
 
